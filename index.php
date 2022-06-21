2d1372d6fdbcb6f46fb18c5822bf9e362f043d34
asmasaeed <asma.saeedh@gmail.com>
2022-06-21 14:16:48 +0800

    Create index.php
    php for index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+<!-- HOME INTRO -->
+          <section class= "container-fluid home-intro">
+            <div class="container">
+    <h2>TAKE A JOURNEY THROUGH OUR NATURAL BIODIVERSITY</h2>
+    <p>Home to several endemic species, WA is home to one of worlds' largest biodiversity hotspot. </p>
+    <a class= "button" href="#" > > </a>
+            </div> <!-- container -->
+        </section>
+
+        <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
