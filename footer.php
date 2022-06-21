e1fec44b776c7226f4fefadc9c44f6e45fbe931e
asmasaeed <asma.saeedh@gmail.com>
2022-06-21 15:53:33 +0800

    Update footer.php
    acf additions for widgets

--- footer.php
+++ footer.php
@@ -3,11 +3,15 @@
 
             <div class="container"><!-- div to hold the content in the middle of the page-->
                 <div class="row"><!-- a row that gives us access to the BS columns-->
-                    <div class="col-md-6 ">
-                        <p>TRAIL SCOUT</p>
+                    <div class="col-md-6 widgetleft">
+                        <!-- <p>TRAIL SCOUT</p> -->
+                        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
                     </div>
                     <div class="col-md-6 ">
-                        <p>Terms and Conditions</p>
+                        <!-- <p>Terms and Conditions</p> -->
+                        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
           </div><!-- row-->
                 </div><!-- container -->
             </div><!-- container-fluid-->
