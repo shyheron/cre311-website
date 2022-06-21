4a216da8367b760ea969d66fc78ff2892cd5b491
asmasaeed <asma.saeedh@gmail.com>
2022-06-21 14:13:01 +0800

    Create footer.php
    php code for footer

---
+++ footer.php
@@ -0,0 +1,19 @@
+<!-- FOOTER -->
+        <footer class="container-fluid"><!-- div for the main image to stretch s to the edge of the page-->
+
+            <div class="container"><!-- div to hold the content in the middle of the page-->
+                <div class="row"><!-- a row that gives us access to the BS columns-->
+                    <div class="col-md-6 ">
+                        <p>TRAIL SCOUT</p>
+                    </div>
+                    <div class="col-md-6 ">
+                        <p>Terms and Conditions</p>
+          </div><!-- row-->
+                </div><!-- container -->
+            </div><!-- container-fluid-->
+        
+        </footer>
+        <?php wp_footer(); ?>
+
+</body>
+</html>
