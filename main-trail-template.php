f570a1ecd3dad6e2bb8f72a5f97fa1c05088bc20
asmasaeed <asma.saeedh@gmail.com>
2022-06-21 14:45:58 +0800

    Create main-trail-template.php
    php for main trail page

---
+++ main-trail-template.php
@@ -0,0 +1,47 @@
+<?php
+    /*
+    Template Name: CV
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+<!-- TRAIL MAP AREAS -->
+<section>
+            <div class="container trails">
+            <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+                    
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1852.jpg" alt="charnley-river-wilderness-camp">
+                    <h4 class="more-title">Charnley River Wilderness Camp</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1853.JPG" alt="mornington-wilderness-camp">
+                    <h4 class="more-title">Mornington Wilderness Camp</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1854.JPG" alt="mt-gibson-wildlife-sanctuary">
+                    <h4 class="more-title">Mt Gibson Wildlife Sanctuary</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1855.jpg" alt="paruna-wildlife-sanctuary">
+                    <h4 class="more-title">Paruna Wildlife Sanctuary</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1856.JPG" alt="karakamia-wildlife-sanctuary">
+                    <h4 class="more-title">Karakamia Wildlife Sanctuary</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="images/IMG_1857.JPG" alt="faure-island">
+                    <h4 class="more-title">Faure Island</h4>
+                    <a class= "button" href="#" > > </a>
+                </div>
+                </div><!-- row-->
+            </div> <!-- container -->
+        </section>
+
+        <?php get_footer(); ?>
