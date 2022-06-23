39bddbb2db68c02af4de20ed32c5d338e5b030a6
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 05:30:47 +0800

    Create paruna-trail-template.php

---
+++ paruna-trail-template.php
@@ -0,0 +1,53 @@
+<?php
+    /*
+    Template Name: Paruna Trail page
+    */
+    ?>
+<?php require_once('paruna-trail-header.php'); ?>
+
+<section class="container-fluid trail-info">
+        <div class="container text-center">
+          <h2 class="text-left"><?php the_field('titleforparunaactivities'); ?></h2>
+          <p class="text-center" ><?php the_field('paragraphforparunaactivities'); ?></p>
+        </div>
+      </section>
+
+      <section class="container-fluid trail-species">
+            <h2 class="text-center"><?php the_field('wildlifetagparuna'); ?></h2>
+            <div class="container">
+              <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+                    
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“bandicoot” src="<?php the_field('parunaspecies1image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies1name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies1description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“dolphin” src="<?php the_field('parunaspecies2image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies2name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies2description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“dugong” src="<?php the_field('parunaspecies3image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies3name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies3description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                  <img class="img-fluid" alt=“finch” src="<?php the_field('parunaspecies4image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies4name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies4description'); ?></p>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“leather-back-turtle” src="<?php the_field('parunaspecies5image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies5name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies5description'); ?></p>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“mangrove-crab” src="<?php the_field('parunaspecies6image');?>">
+                    <p class="text-center" ><?php the_field('parunaspecies6name'); ?></p>
+                    <p class="text-center" ><?php the_field('parunaspecies6description'); ?></p>
+              </div>
+            </div>
+          </section>
+
+        <?php get_footer(); ?>
