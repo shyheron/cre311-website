83be7dae03b325abb31056f1e6cf237d813b8f19
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 01:05:09 +0800

    Update main-trail-template.php

--- main-trail-template.php
+++ main-trail-template.php
@@ -1,22 +1,9 @@
 <?php
     /*
-    Template Name: Main trail
+    Template Name: Trail Scout Main Trail page
     */
     ?>
 <?php require_once('main-trail-header.php'); ?>
-
-<!-- TRAIL INTRODUCTION -->
-<div class="col-md-4 trail-map-description">
-            <h3 class="text-center"><?php the_field('titleformaintrail'); ?></h3>
-            <p class="text-center" ><?php the_field('paragraphformaintrailintro'); ?></p>
-        </div>
-        <div class="col-md-8">
-        <img class="img-fluid" alt=“portait” src="<?php the_field('mapimage');?>">
-        </div>
-    </div>    
-
-          </div><!-- container-->
-      </header>
 
         <!-- TRAIL MAP AREAS -->
         <section class="trails">
@@ -34,24 +21,24 @@
                     <a href="<?php the_field('trail2button'); ?>" class="button"> > </a>
                 </div>
                 <div class="col-md-4 trailimg">
-                    <img class="img-fluid" alt=“mt-gibson-wildlife-sanctuary” src="<?php the_field('trail2image');?>">
-                    <p class="text-center" ><?php the_field('trail2name'); ?></p>
-                    <a href="<?php the_field('trail2button'); ?>" class="button"> > </a>
+                    <img class="img-fluid" alt=“mt-gibson-wildlife-sanctuary” src="<?php the_field('trail3image');?>">
+                    <p class="text-center" ><?php the_field('trail3name'); ?></p>
+                    <a href="<?php the_field('trail3button'); ?>" class="button"> > </a>
                 </div>
                 <div class="col-md-4 trailimg">
-                    <img class="img-fluid" alt=“paruna-wildlife-sanctuary” src="<?php the_field('trail2image');?>">
-                    <p class="text-center" ><?php the_field('trail2name'); ?></p>
-                    <a href="<?php the_field('trail2button'); ?>" class="button"> > </a>
+                    <img class="img-fluid" alt=“paruna-wildlife-sanctuary” src="<?php the_field('trail4image');?>">
+                    <p class="text-center" ><?php the_field('trail4name'); ?></p>
+                    <a href="<?php the_field('trail4button'); ?>" class="button"> > </a>
                 </div>
                 <div class="col-md-4 trailimg">
-                    <img class="img-fluid" alt=“karakamia-wildlife-sanctuary” src="<?php the_field('trail2image');?>">
-                    <p class="text-center" ><?php the_field('trail2name'); ?></p>
-                    <a href="<?php the_field('trail2button'); ?>" class="button"> > </a>
+                    <img class="img-fluid" alt=“karakamia-wildlife-sanctuary” src="<?php the_field('trail5image');?>">
+                    <p class="text-center" ><?php the_field('trail5name'); ?></p>
+                    <a href="<?php the_field('trail5button'); ?>" class="button"> > </a>
                 </div>
                 <div class="col-md-4 trailimg">
-                    <img class="img-fluid" alt=“broome” src="<?php the_field('trail2image');?>">
-                    <p class="text-center" ><?php the_field('trail2name'); ?></p>
-                    <a href="<?php the_field('trail2button'); ?>" class="button"> > </a>
+                    <img class="img-fluid" alt=“broome” src="<?php the_field('trail6image');?>">
+                    <p class="text-center" ><?php the_field('trail6name'); ?></p>
+                    <a href="<?php the_field('trail6button'); ?>" class="button"> > </a>
                 </div>
                 </div><!-- row-->
             </div> <!-- container -->
