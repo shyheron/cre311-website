84d32cd8d3cfb4f363346307513490310b3a62b7
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 05:30:25 +0800

    Create broome-trail-header.php

---
+++ broome-trail-header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap" rel="stylesheet">
+
+<title>
+            <?php
+              if (function_exists('is_tag') && is_tag()) {
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+              elseif (is_archive()) {
+                 wp_title(''); echo ' Archive - '; }
+              elseif (is_search()) {
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+              elseif (!(is_404()) && (is_single()) || (is_page())) {
+                 wp_title(''); echo ' - '; }
+              elseif (is_404()) {
+                 echo 'Not Found - '; }
+              if (is_home()) {
+                 bloginfo('name'); echo ' - '; bloginfo('description'); }
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) {
+                 echo ' - page '. $paged; }
+           ?>
+</title>
+<meta name="description" content="<?php bloginfo('description'); ?>">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+</head>
+<body>
+
+    <header class="container-fluid trail-map">   <!-- div for the main image the stretches to the edge of the page- -->
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">Navbar</a>
+      
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="row text-left"><!--  a row that gives us access to the BS columns-->
+                    
+                    <div class="col-md-4 trail-map-description">
+                      <h1 class="text-left"><?php the_field('titleforbroome'); ?></h1>
+                      <p class="text-left" ><?php the_field('paragraphforbroomeintro'); ?></p>
+                    </div>
+                    <div class="col-md-8">
+                    <img class="img-fluid" alt=“trail-image” src="<?php the_field('broomeimage');?>">
+                    </div>
+                </div>    
+          
+          
+          <?php wp_head(); ?> <!-- very important that this is added--> 
+        
+        </header>
