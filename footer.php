50cb2aa21199b0948762fba84bfee4c605ab8a0e
asmasaeed <asma.saeedh@gmail.com>
2022-06-23 23:57:56 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,7 +12,8 @@ endif; ?>
                     <div class="col-md-6 ">
                         <!-- <p>Terms and Conditions</p> -->
                         <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
-          </div><!-- row-->
+endif; ?>
+ </div><!-- row-->
                 </div><!-- container -->
             </div><!-- container-fluid-->
         
