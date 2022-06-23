934aba12ff223378551228dd5b84feb8a435b346
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 05:30:55 +0800

    Create mornington-trail-template.php

---
+++ mornington-trail-template.php
@@ -0,0 +1,53 @@
+<?php
+    /*
+    Template Name: Mornington Trail page
+    */
+    ?>
+<?php require_once('mornington-trail-header.php'); ?>
+
+<section class="container-fluid trail-info">
+        <div class="container text-center">
+          <h2 class="text-left"><?php the_field('titleformorningtonactivities'); ?></h2>
+          <p class="text-center" ><?php the_field('paragraphformorningtonactivities'); ?></p>
+        </div>
+      </section>
+
+      <section class="container-fluid trail-species">
+            <h2 class="text-center"><?php the_field('wildlifetagmornington'); ?></h2>
+            <div class="container">
+              <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+                    
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“bandicoot” src="<?php the_field('morningtonspecies1image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies1name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies1description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“dolphin” src="<?php the_field('morningtonspecies2image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies2name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies2description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" alt=“dugong” src="<?php the_field('morningtonspecies3image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies3name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies3description'); ?></p>
+                </div>
+                <div class="col-md-4">
+                  <img class="img-fluid" alt=“finch” src="<?php the_field('morningtonspecies4image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies4name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies4description'); ?></p>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“leather-back-turtle” src="<?php the_field('morningtonspecies5image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies5name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies5description'); ?></p>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“mangrove-crab” src="<?php the_field('morningtonspecies6image');?>">
+                    <p class="text-center" ><?php the_field('morningtonspecies6name'); ?></p>
+                    <p class="text-center" ><?php the_field('morningtonspecies6description'); ?></p>
+              </div>
+            </div>
+          </section>
+
+        <?php get_footer(); ?>
