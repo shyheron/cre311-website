07deb9375077dae04a2d4a915fef1d47568441cb
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 00:16:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
 
 <!-- HOME INTRO -->
-         <section class= "container-fluid home-intro">
-            <div class="container">
+
     <h2><?php the_field('titleforintro'); ?></h2><!--  custom field for the mini introduction content-->
         <p class="about-text"><?php the_field('paragraphforintro'); ?></p>
         <a href="<?php the_field('buttonformoreinfo'); ?>" class="button"> > </a>
