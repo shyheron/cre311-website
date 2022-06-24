561650c1cb9e81936d1e9c8f52e4fa60a1f4c1f4
asmasaeed <asma.saeedh@gmail.com>
2022-06-24 12:09:01 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,6 +8,9 @@
 <!-- HOME INTRO -->
           <section class= "container-fluid home-intro">
             <div class="container">
+            <h2><?php the_field('titleforintro'); ?></h2><!--  custom field for the mini introduction content-->
+        <p class="about-text"><?php the_field('paragraphforintro'); ?></p>
+        <a href="<?php the_field('buttonformoreinfo'); ?>" class="button"> > </a>
 <!--     <h2>TAKE A JOURNEY THROUGH OUR NATURAL BIODIVERSITY</h2>
     <p>Home to several endemic species, WA is home to one of worlds' largest biodiversity hotspot. </p>
     <a class= "button" href="#" > > </a> -->
